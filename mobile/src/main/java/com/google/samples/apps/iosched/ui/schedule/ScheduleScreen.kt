da27e0d76c68437ea81789bf374a8adce27572be
iamarjun <arjun.manoj@practo.com>
2021-05-30 23:48:32 +0530

    Segregate list into days then into time slots

--- mobile/src/main/java/com/google/samples/apps/iosched/ui/schedule/ScheduleScreen.kt
+++ mobile/src/main/java/com/google/samples/apps/iosched/ui/schedule/ScheduleScreen.kt
@@ -16,10 +16,6 @@
 
 package com.google.samples.apps.iosched.ui.schedule
 
-import android.graphics.Typeface
-import android.text.SpannableStringBuilder
-import android.text.style.AbsoluteSizeSpan
-import android.text.style.StyleSpan
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -35,19 +31,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.core.text.inSpans
-import com.google.accompanist.insets.ProvideWindowInsets
 import com.google.accompanist.insets.statusBarsPadding
 import com.google.samples.apps.iosched.R
 import com.google.samples.apps.iosched.model.ConferenceDay
 import com.google.samples.apps.iosched.model.Tag
 import com.google.samples.apps.iosched.model.userdata.UserSession
+import com.google.samples.apps.iosched.shared.util.TimeUtils
 import com.google.samples.apps.iosched.ui.MainActivityViewModel
 import com.google.samples.apps.iosched.ui.theme.*
+import timber.log.Timber
 import java.time.ZoneId
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
@@ -56,9 +53,6 @@ import java.util.*
 
 private val timeFormatter = DateTimeFormatter.ofPattern("h:mm")
 private val meridiemFormatter = DateTimeFormatter.ofPattern("a")
-private val timeTextSizeSpan = AbsoluteSizeSpan(20)
-private val meridiemTextSizeSpan = AbsoluteSizeSpan(14)
-private val boldSpan = StyleSpan(Typeface.BOLD)
 
 @ExperimentalFoundationApi
 @ExperimentalComposeUiApi
@@ -70,99 +64,100 @@ fun ScheduleScreen(
 ) {
     //TODO: Once the full migration is done, move this to the top level
     IOTheme {
-        ProvideWindowInsets {
-            Scaffold(
-                scaffoldState = scaffoldState,
-                topBar = {
-                    val days = mutableListOf<DayIndicator>(
-                        DayIndicator(
-                            day = ConferenceDay(
-                                start = ZonedDateTime.now(),
-                                end = ZonedDateTime.now().plusDays(1),
-                            )
-                        ),
-                        DayIndicator(
-                            day = ConferenceDay(
-                                start = ZonedDateTime.now().plusDays(1),
-                                end = ZonedDateTime.now().plusDays(2)
-                            )
-                        ),
-                        DayIndicator(
-                            day = ConferenceDay(
-                                start = ZonedDateTime.now().plusDays(2),
-                                end = ZonedDateTime.now().plusDays(3)
-                            )
+        Scaffold(
+            scaffoldState = scaffoldState,
+            topBar = {
+                val days = mutableListOf<DayIndicator>(
+                    DayIndicator(
+                        day = ConferenceDay(
+                            start = ZonedDateTime.now(),
+                            end = ZonedDateTime.now().plusDays(1),
+                        )
+                    ),
+                    DayIndicator(
+                        day = ConferenceDay(
+                            start = ZonedDateTime.now().plusDays(1),
+                            end = ZonedDateTime.now().plusDays(2)
+                        )
+                    ),
+                    DayIndicator(
+                        day = ConferenceDay(
+                            start = ZonedDateTime.now().plusDays(2),
+                            end = ZonedDateTime.now().plusDays(3)
                         )
                     )
-
-                    val listState = rememberLazyListState()
-                    var selectedIndex by remember { mutableStateOf(-1) }
-                    TopAppBar(
-                        title = {
-                            LazyRow(
-                                state = listState
-                            ) {
-                                itemsIndexed(days) { index, item ->
-                                    Surface(
-                                        color = if (index == selectedIndex) Teal else Transparent,
-                                        shape = RoundedCornerShape(50)
-                                    ) {
-                                        Text(
-                                            text = "Day ${index + 1}",
-                                            modifier = Modifier
-                                                .selectable(
-                                                    selected = index == selectedIndex,
-                                                    onClick = {
-                                                        selectedIndex = if (selectedIndex != index)
-                                                            index else -1
-                                                    }
-                                                )
-                                                .padding(
-                                                    horizontal = 16.dp,
-                                                    vertical = 8.dp
-                                                ),
-                                            style = TextStyle(
-                                                color = if (index == selectedIndex) White else Black,
-                                            )
-                                        )
-                                    }
-
-                                }
-                            }
-                        },
-                        actions = {
-
-                            IconButton(onClick = { mainViewModel.onProfileClicked() }) {
-                                Icon(
-                                    painter = painterResource(id = R.drawable.ic_default_profile_avatar),
-                                    contentDescription = "Profile",
-                                    tint = MaterialTheme.colors.primary
-                                )
-                            }
-                        },
-                        backgroundColor = White,
-                        contentColor = Black,
-                        elevation = 8.dp,
-                        modifier = Modifier.statusBarsPadding()
-                    )
-                }
-            ) {
-
-                val modifier = Modifier.padding(it)
-
-
+                )
                 val scheduleUiData by viewModel.scheduleUiData.collectAsState()
+                val listState = rememberLazyListState()
+                var selectedIndex by remember { mutableStateOf(-1) }
+                TopAppBar(
+                    title = {
+                        LazyRow(
+                            state = listState
+                        ) {
+                            itemsIndexed(
+                                scheduleUiData.dayIndexer?.days ?: days
+                            ) { index, item ->
+                                Surface(
+                                    color = if (index == selectedIndex) Teal else Transparent,
+                                    shape = RoundedCornerShape(50)
+                                ) {
+                                    Text(
+                                        text = "Day ${index + 1}",
+                                        modifier = Modifier
+                                            .selectable(
+                                                selected = index == selectedIndex,
+                                                onClick = {
+                                                    selectedIndex = if (selectedIndex != index)
+                                                        index else -1
+                                                }
+                                            )
+                                            .padding(
+                                                horizontal = 16.dp,
+                                                vertical = 8.dp
+                                            ),
+                                        style = TextStyle(
+                                            color = if (index == selectedIndex) White else Black,
+                                        )
+                                    )
+                                }
 
-                if (scheduleUiData.isLoading())
-                    CircularProgressIndicator()
-                else
-                    Schedules(
-                        modifier = modifier,
-                        schedules = scheduleUiData.list!!,
-                        zoneId = scheduleUiData.timeZoneId!!,
-                    )
+                            }
+                        }
+                    },
+                    actions = {
 
+                        IconButton(onClick = { mainViewModel.onProfileClicked() }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_default_profile_avatar),
+                                contentDescription = "Profile",
+                                tint = MaterialTheme.colors.primary
+                            )
+                        }
+                    },
+                    backgroundColor = White,
+                    contentColor = Black,
+                    elevation = 8.dp,
+                    modifier = Modifier.statusBarsPadding()
+                )
             }
+        ) {
+
+            val modifier = Modifier.padding(it)
+
+
+            val scheduleUiData by viewModel.scheduleUiData.collectAsState()
+            val loading by viewModel.isLoading.collectAsState()
+
+            if (loading)
+                CircularProgressIndicator()
+            else
+                Schedules(
+                    modifier = modifier,
+                    schedules = scheduleUiData.list!!,
+                    zoneId = scheduleUiData.timeZoneId!!,
+                )
+
         }
     }
 
@@ -183,41 +178,81 @@ private fun Schedules(
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
 
-        val sessions = schedules.groupBy { it.session.startTime.truncatedTo(ChronoUnit.MINUTES) }
+        val isConferenceTimeZone = TimeUtils.isConferenceTimeZone(zoneId = zoneId)
 
-        sessions.forEach { (startTime, sessions) ->
+        val days = schedules.groupBy {
+            TimeUtils.getLabelResForTime(it.session.startTime, isConferenceTimeZone)
+        }
 
-            stickyHeader {
-                val text = SpannableStringBuilder().apply {
-                    inSpans(timeTextSizeSpan) {
-                        append(timeFormatter.format(startTime))
+        Timber.d("$days")
+
+        days.forEach { day, sessions ->
+            item {
+                ScheduleHeader(
+                    modifier = modifier,
+                    stringRes = day
+                )
+            }
+
+            val userSession = sessions.groupBy {
+                TimeUtils.zonedTime(it.session.startTime, zoneId).truncatedTo(ChronoUnit.MINUTES)
+            }
+
+            userSession.forEach { (startTime, list) ->
+
+                stickyHeader {
+                    Column(
+                        modifier = modifier.width(84.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = timeFormatter.format(startTime),
+                            style = MaterialTheme.typography.h6.copy(
+                                color = MaterialTheme.colors.primary,
+                            )
+                        )
+                        Text(
+                            text = meridiemFormatter.format(startTime)
+                                .toUpperCase(Locale.getDefault()),
+                            style = MaterialTheme.typography.body2.copy(
+                                color = MaterialTheme.colors.primary,
+                                fontWeight = FontWeight.SemiBold
+                            )
+                        )
                     }
-                    append(System.lineSeparator())
-                    inSpans(meridiemTextSizeSpan, boldSpan) {
-                        append(meridiemFormatter.format(startTime).toUpperCase(Locale.getDefault()))
-                    }
+
                 }
 
-                Text(
-                    modifier = modifier.width(84.dp),
-                    text = text.toString(),
-                    color = MaterialTheme.colors.primary,
-                    textAlign = TextAlign.Center
-                )
-            }
+                items(list) { session ->
+                    Schedule(
+                        modifier = modifier,
+                        userSession = session,
+                        zoneId = zoneId
+                    )
+                }
 
-            items(sessions) { session ->
-                Schedule(
-                    modifier = modifier,
-                    userSession = session,
-                    zoneId = zoneId
-                )
             }
-
         }
     }
 
 }
+
+@Composable
+private fun ScheduleHeader(
+    modifier: Modifier,
+    stringRes: Int
+) {
+    Text(
+        modifier = modifier
+            .padding(top = 32.dp, bottom = 16.dp)
+            .fillMaxWidth(),
+        text = stringResource(id = stringRes),
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.h6
+    )
+}
+
 
 @ExperimentalComposeUiApi
 @Composable
@@ -229,65 +264,73 @@ private fun Schedule(
     Column(
         modifier = modifier.padding(start = 84.dp, end = 16.dp)
     ) {
-
+        Text(
+            text = userSession.session.title,
+            style = MaterialTheme.typography.body1.copy(fontWeight = FontWeight.W600)
+        )
         Row(
-            modifier = modifier,
-            verticalAlignment = Alignment.CenterVertically,
+            modifier = modifier
         ) {
-            Column(
-                modifier = modifier.weight(1f),
-                horizontalAlignment = Alignment.Start,
-            ) {
-                Text(
-                    text = userSession.session.title,
-                    style = MaterialTheme.typography.body1.copy(fontWeight = FontWeight.W600)
-                )
-                Row(
-                    modifier = modifier
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_reserved),
-                        contentDescription = null,
-                        tint = MaterialTheme.colors.primary,
-                    )
-                    Spacer(modifier = modifier.width(10.dp))
-                    Text(
-                        text = "RESERVE SEAT",
-                        style = MaterialTheme.typography.subtitle2.copy(
-                            color = Color.Gray
-                        )
-                    )
-                }
-
-                Row(
-                    modifier = modifier
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_livestreamed),
-                        contentDescription = null,
-                        tint = MaterialTheme.colors.primary,
-                    )
-                    Spacer(modifier = modifier.width(10.dp))
-                    Text(
-                        text = sessionDateTimeLocation(
-                            userSession.session.startTime,
-                            zoneId,
-                            false,
-                            userSession.session.room
-                        ),
-                        style = MaterialTheme.typography.subtitle2.copy(
-                            color = Color.Gray
-                        )
-                    )
-                }
-            }
-
             Icon(
-                modifier = modifier.size(24.dp),
-                painter = painterResource(id = R.drawable.ic_agenda_keynote),
-                contentDescription = null
+                painter = painterResource(id = R.drawable.ic_livestreamed),
+                contentDescription = null,
+                tint = Color.Gray,
+            )
+            Spacer(modifier = modifier.width(10.dp))
+            Text(
+                text = sessionDateTimeLocation(
+                    userSession.session.startTime,
+                    zoneId,
+                    true,
+                    userSession.session.room
+                ),
+                style = MaterialTheme.typography.subtitle2.copy(
+                    color = Color.Gray
+                )
             )
         }
+
+//        Row(
+//            modifier = modifier,
+//            verticalAlignment = Alignment.CenterVertically,
+//        ) {
+//            Column(
+//                modifier = modifier.weight(1f),
+//                horizontalAlignment = Alignment.Start,
+//            ) {
+//                Text(
+//                    text = userSession.session.title,
+//                    style = MaterialTheme.typography.body1.copy(fontWeight = FontWeight.W600)
+//                )
+//                Row(
+//                    modifier = modifier
+//                ) {
+//                    Icon(
+//                        painter = painterResource(id = R.drawable.ic_livestreamed),
+//                        contentDescription = null,
+//                        tint = MaterialTheme.colors.primary,
+//                    )
+//                    Spacer(modifier = modifier.width(10.dp))
+//                    Text(
+//                        text = sessionDateTimeLocation(
+//                            userSession.session.startTime,
+//                            zoneId,
+//                            false,
+//                            userSession.session.room
+//                        ),
+//                        style = MaterialTheme.typography.subtitle2.copy(
+//                            color = Color.Gray
+//                        )
+//                    )
+//                }
+//            }
+//
+//            Icon(
+//                modifier = modifier.size(24.dp),
+//                painter = painterResource(id = R.drawable.ic_agenda_keynote),
+//                contentDescription = null
+//            )
+//        }
 //        Row {
 //            userSession.session.displayTags.forEach {
 //                Tag(
