6f7b5d4c60e8a6af84618161b20aba0c738d59ea
iamarjun <arjun.manoj@practo.com>
2021-07-30 20:10:52 +0530

    Bump to 24 for minSDK for the time being

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -21,19 +21,7 @@ object Versions {
 
     const val COMPILE_SDK = 30
     const val TARGET_SDK = 30
-    const val MIN_SDK = 21
+    const val MIN_SDK = 24
 
-    const val ANDROID_GRADLE_PLUGIN = "7.0.0-beta03"
     const val BENCHMARK = "1.0.0"
-    const val COMPOSE = "1.0.0-beta04"
-    const val FIREBASE_CRASHLYTICS = "2.3.0"
-    const val GOOGLE_SERVICES = "4.3.3"
-    const val KOTLIN = "1.4.32"
-    const val NAVIGATION = "2.3.5"
-    const val HILT_AGP = "2.36"
-
-    // TODO: Remove this once the version for
-    //  "org.threeten:threetenbp:${Versions.threetenbp}:no-tzdb" using java-platform in the
-    //  depconstraints/build.gradle.kts is defined
-    const val THREETENBP = "1.3.6"
 }
